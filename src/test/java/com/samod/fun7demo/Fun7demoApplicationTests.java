ca514f57789cb462012dbad89d256d5784934abf
Saemu5 <wotyla@gmail.com>
2020-05-11 23:08:10 +0200

    init

---
+++ src/test/java/com/samod/fun7demo/Fun7demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samod.fun7demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fun7demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
